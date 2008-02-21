a5eaecaa89225556991a9e345dd25984a80f6389
Ole Nielsen <ole.moller.nielsen@gmail.com>
2008-02-21 07:07:35 +0000

    Updated demo3.py for pypar version 2 thanks to Felix Richter.

--- demos/demo3.py
+++ demos/demo3.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 """
-Master/Slave Parallel decomposition sample 
- 
-Run as 
+Master/Slave Parallel decomposition sample
+
+Run as
    python demo3.py
-or 
+or
    mpirun -np 2 demo3.py
   (perhaps try number of processors more than 2)
   
@@ -15,14 +15,14 @@ OMN, GPC FEB 2002
 import sys
 
 try:
-  import numpy
+    import numpy
 except:
-  raise Exception, 'Module numpy must be present to run pypar'
+    raise Exception, 'Module numpy must be present to run pypar'
   
 try:
-  import pypar
+    import pypar
 except:
-  raise Exception, 'Module pypar must be present to run parallel'
+    raise Exception, 'Module pypar must be present to run parallel'
 
 
 print 'Modules numpy, pypar imported OK'
@@ -33,94 +33,90 @@ DIETAG =  2
 
 def master():
     numCompleted = 0
-    
+
     print '[MASTER]: I am processor %d of %d on node %s'\
           %(MPI_myid, MPI_numproc, MPI_node)
-    
+
     # start slaves distributing the first work slot
-    for i in range(1, min(MPI_numproc, numWorks)): 
+    for i in range(1, min(MPI_numproc, numWorks)):
         work = workList[i]
-        pypar.raw_send(work, i, WORKTAG) 
+        pypar.send(work, destination=i, tag=WORKTAG)
         print '[MASTER]: sent work "%s" to node %d' %(work, i)
 
-    # dispach the remaining work slots on dynamic load-balancing policy
+    # dispatch the remaining work slots on dynamic load-balancing policy
     # the quicker to do the job, the more jobs it takes
     for work in workList[MPI_numproc:]:
-        result = '  '
-        err, status = pypar.raw_receive(result, pypar.any_source,
-                                        pypar.any_tag, return_status=True) 
+        result, status = pypar.receive(source=pypar.any_source, tag=WORKTAG,
+                                       return_status=True)
+
         print '[MASTER]: received result "%s" from node %d'\
               %(result, status.source)
-        
+
         numCompleted += 1
-        pypar.raw_send(work, status.source, WORKTAG)
+        pypar.send(work, destination=status.source, tag=WORKTAG)
         print '[MASTER]: sent work "%s" to node %d' %(work, status.source)
-    
+
     # all works have been dispatched out
     print '[MASTER]: toDo : %d' %numWorks
     print '[MASTER]: done : %d' %numCompleted
-    
-    # I've still to take into the remaining completions   
-    while(numCompleted < numWorks): 
-        result = '  '
-        err, status = pypar.raw_receive(result, pypar.any_source,
-                                        pypar.any_tag, return_status=True) 
+
+    # I've still to take into the remaining completions
+    while (numCompleted < numWorks):
+        result, status = pypar.receive(source=pypar.any_source,
+                                       tag=WORKTAG,
+                                       return_status=True)
         print '[MASTER]: received (final) result "%s" from node %d'\
                   %(result, status.source)
         numCompleted += 1
         print '[MASTER]: %d completed' %numCompleted
-        
+
     print '[MASTER]: about to terminate slaves'
 
     # Tell slaves to stop working
-    for i in range(1, MPI_numproc): 
-        pypar.raw_send('#', i, DIETAG) 
-        print '[MASTER]: sent (final) work "%s" to node %d' %(0, i)
-        
+    for i in range(1, MPI_numproc):
+        pypar.send('#', destination=i, tag=DIETAG)
+        print '[MASTER]: sent termination signal to node %d' %(i, )
+
     return
-    
+
 def slave():
 
     print '[SLAVE %d]: I am processor %d of %d on node %s'\
                      %(MPI_myid, MPI_myid, MPI_numproc, MPI_node)
 
     while True:
-        result = ' '
-        err, status = pypar.raw_receive(result, pypar.any_source,
-                                        pypar.any_tag, return_status=True) 
+        result, status = pypar.receive(source=0,
+                                       tag=pypar.any_tag,
+                                       return_status=True)
         print '[SLAVE %d]: received work "%s" with tag %d from node %d'\
                   %(MPI_myid, result, status.tag, status.source)
-       
+
         if (status.tag == DIETAG):
             print '[SLAVE %d]: received termination from node %d'\
                              %(MPI_myid, 0)
             return
         else:
             result = 'X'+result
-            pypar.raw_send(result, 0)
+            pypar.send(result, destination=0, tag=WORKTAG)
             print '[SLAVE %d]: sent result "%s" to node %d'\
             %(MPI_myid, result, 0)
-            
-       
+
+
 
 if __name__ == '__main__':
     MPI_myid =    pypar.rank()
     MPI_numproc = pypar.size()
-    MPI_node =    pypar.Get_processor_name()
+    MPI_node =    pypar.get_processor_name()
 
-    _workList = ('_dummy_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
-    workList = ('_dummy_', 'a', 'b', 'c')
+    workList = ('_dummy_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
     numWorks = len(workList) - 1
-    
-    
+
     #FIXME, better control here
     if MPI_numproc > numWorks or MPI_numproc < 2:
-        pypar.Finalize()
-	if MPI_myid == 0:
-          print 'ERROR: Number of processors must be in the interval [2,%d].'\
-                %numWorks
-	  
-        sys.exit(-1)
+        pypar.finalize()
+        if MPI_myid == 0:
+              print 'ERROR: Number of processors must be in the interval [2,%d].'%numWorks
+              sys.exit(-1)
 
     if MPI_myid == 0:
         master()
@@ -129,4 +125,4 @@ if __name__ == '__main__':
 
     pypar.finalize()
     print 'MPI environment finalized.'
-        	
+    
